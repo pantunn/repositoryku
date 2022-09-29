d641867a0b6dd0157db8a4492a50246e5ce19c59
pantunelfreddy <pantun.elfreddy@ui.ac.id>
2022-09-29 16:23:28 +0700

    mengerjakan lab 04

--- wishlist/views.py
+++ wishlist/views.py
@@ -63,6 +63,7 @@ def login_user(request):
     context = {}
     return render(request, 'login.html', context)
 
+
 def logout_user(request):
     logout(request)
     response = HttpResponseRedirect(reverse('wishlist:login'))
